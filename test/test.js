0b25a106c323ea3a5b0d77248e55fbe30d15c325
Mikko Lehtinen <mikko+gh@applifier.com>
2014-05-02 16:35:51 +0300

    Simplify

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 var assert = require('assert');
-var when = require('when');
 var slugify = require('..');
 
 describe('non-latin slugs', function() {
@@ -7,20 +6,17 @@ describe('non-latin slugs', function() {
   it('should slugify different languages', function() {
     var tests = {
       "I ♥ latin@": "i-love-latin-",
-      "Я люблю русский": "ya-lyublyu-russkij",
-      "私は ひらがな が大好き": "ha-hiragana-gaki",
-      "我爱官话": "wo3-ai4-guan1-hua4",
-      "안녕하세요": "annyeonghaseyo"
+      "Я люблю русский": "ia-liubliu-russkii",
+      "私は ひらがな が大好き": "si-ha-hiragana-gada-hao-ki",
+      "我爱官话": "wo-ai-guan-hua",
+      "안녕하세요": "annyeonghaseyo",
+      "ÆÉç": "aeec"
     };
     var testKeys = Object.keys(tests);
     var fns = [];
-    testKeys.forEach(function(test) {
-      fns.push(slugify(test));
-    });
-    return when.all(fns).then(function(results) {
-      results.forEach(function(res, idx) {
-        assert.equal(res, tests[testKeys[idx]]);
-      });
+    testKeys.forEach(function(test, idx) {
+      var slug = slugify(test);
+      assert.equal(slug, tests[testKeys[idx]]);
     });
   });
 
